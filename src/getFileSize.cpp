0ccd2b0b2006f8d9145a54a79cd846f734260c21
HojdaAdelin <adelinionut297@gmail.com>
2023-08-28 17:11:59 +0300

    Set size

--- src/getFileSize.cpp
+++ src/getFileSize.cpp
@@ -2,10 +2,24 @@
 #include "clever++.h"
 #include <string>
 #include <Windows.h>
+#include <fstream>
 
 std::string CleverCpp::fileSize(std::string filetosize) {
 
     unsigned long long getSize = GetFileSizeEx(CreateFileA(filetosize.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL), NULL);
     return std::to_string(getSize);
+
+}
+
+void CleverCpp::setSize(std::string filetosetsize, int size) {
+
+    std::ofstream setSizeToFile(filetosetsize, std::ios::binary | std::ios::out);
+
+    std::streamsize newSize = static_cast<std::streamsize>(size) * 1024 * 1024;
+
+    setSizeToFile.seekp(newSize - 1);
+    setSizeToFile.write("\0", 1);
+
+    setSizeToFile.close();
 
 }
