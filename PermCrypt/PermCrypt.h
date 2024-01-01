623711bcc9214e1e2109ade72a4df2f3be83289d
HojdaAdelin <adelinionut297@gmail.com>
2024-01-01 21:16:42 +0200

    New

---
+++ PermCrypt/PermCrypt.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+
+class permEncrypt {
+
+private:
+	std::string encryptedText;
+
+public:
+	permEncrypt(std::string text, int type) {
+
+
+
+	}
+
+	std::string getEncryptedText() const {
+
+		return encryptedText;
+
+	}
+
+private:
+
+
+};
